c531dcb70aa0d75aaf99282aba118bf877e8cb64
Gabor Fekete <feketegabor98@gmail.com>
2024-04-12 23:26:44 +0200

    Finished web scraping exercise

--- web.py
+++ web.py
@@ -4,6 +4,7 @@ import bs4
 page = requests.get('https://quotes.toscrape.com/')
 soup = bs4.BeautifulSoup(page.text, "lxml")
 
+# Get all the authors' names on the first page
 authors = soup.select('.author')
 authors_set = set()
 for author in authors:
@@ -13,5 +14,30 @@ for author in sorted(authors_set):
     print(author)
 print()
 
-quotes = soup.select('.quote')
-print(type(quotes))
+# Get all the quotes on the first page
+quotes = soup.select('.quote .text')
+for quote in quotes:
+    print(quote.text)
+print()
+
+# Get the top tags from the right side
+tags = soup.select('.tag-item .tag')
+for tag in tags:
+    print(tag.text)
+
+# Get all the unique authors from all pages
+webpage = 'https://quotes.toscrape.com/page/{}/'
+page = 1
+authors_set = set()
+while True:
+    content = requests.get(webpage.format(page))
+    if 'No quotes found!' in content.text:
+        break
+    soup = bs4.BeautifulSoup(content.text, "lxml")
+    authors = soup.select('.author')
+    for author in authors:
+        authors_set.add(author.text)
+    page+=1
+
+for author in sorted(authors_set):
+    print(author)
